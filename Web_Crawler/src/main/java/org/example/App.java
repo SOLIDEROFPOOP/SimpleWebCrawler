0d29be203d10cbdf6f23b1699eae7c94faf0bef8
Kapparov Murat <paprim2003@gmail.com>
2024-07-15 21:20:17 +0500

    simple wikipedia crawler

--- Web_Crawler/src/main/java/org/example/App.java
+++ Web_Crawler/src/main/java/org/example/App.java
@@ -1,13 +1,53 @@
 package org.example;
 
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.awt.*;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    private final static String url = "https://en.wikipedia.org/";
+    public static void main( String[] args ) {
+        crawl(1,url, new ArrayList<String>());
+    }
+
+    private static void crawl(int level, String url, ArrayList<String> visited) {
+        if (level <= 5){
+            Document doc = request(url,visited);
+            if (doc != null){
+                for (Element link : doc.select("a[href]")){
+                    String next_link = link.absUrl("href");
+                    if (visited.contains(next_link) == false){
+                        crawl(level++, next_link, visited);
+                    }
+                }
+            }
+        }
+
+    }
+    private static Document request(String url, ArrayList<String> v){
+        try {
+            Connection con = Jsoup.connect(url);
+            Document doc = con.get();
+            if (con.response().statusCode() == 200){
+                System.out.println("Link " + url + " " + " statuscode: " + con.response().statusCode());
+                System.out.println(doc.title());
+                v.add(url);
+                return doc;
+            }
+            return null;
+        } catch (IOException e){
+            e.printStackTrace();
+            return null;
+        }
+
     }
 }
